3ad0f5287adcb807d05fe3748f4a35fdfc628b9e
Ananis93 <ananis93@hotmail.com>
2013-09-25 22:20:59 -0500

    first commit

---
+++ promedio.php
@@ -0,0 +1,15 @@
+<?php
+$calificaciones = array("95.5", "60.0", "86.7", "90.2", "78.5", "50.0", "65.4", "89.9", "91.4", "75.6", "50.0", "88.4", "90.1", "89.0", "70.0", "100.0", "83.4", "77.4", "67.2", "50.0");
+$B=0;
+
+echo "Las Calificaciones Son:"
+For($i=0; $i < count($calificaciones); $i++){
+	echo " ".$calificaciones[$i]." ";
+	}
+echo "El promedio es: "
+For($a=0; $a < count($calificaciones); $a++){
+	$B = $calificaciones[$i] + B;
+	}
+echo $B;
+
+?>
